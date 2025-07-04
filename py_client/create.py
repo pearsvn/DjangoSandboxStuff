e06a203cb261e5bcaec8deb9445dc29807bd27a5
Alfie Pearson <alfie@Mac.communityfibre.co.uk>
2025-07-04 14:51:25 +0100

    create and test with >1 user

--- py_client/create.py
+++ py_client/create.py
@@ -7,20 +7,22 @@ load_dotenv()
 
 endpoint = "http://127.0.0.1:8080/api/products/"
 payload = {
-    'title': 'The Nth Item',
-    'content': 'Nth Item Content',
-    'price': 9.99,
+    'title': 'My Item',
+    'content': 'My Content',
+    'price': 119.99,
 }
 
-token = os.getenv('API_TOKEN')
+username = os.getenv('API_USERNAME', 'testuser')
+token = os.getenv(f'{username.upper()}_TOKEN')
 headers = {'Authorization': f'Token {token}'}
 
-post_response = requests.post(endpoint, json=payload)
+post_response = requests.post(endpoint, json=payload, headers=headers)
 
 try:
     response_json = post_response.json()
-    print(json.dumps(response_json, indent=4))
+    print(f"Request Headers: {headers}")
+    print("Request Payload: ", json.dumps(response_json, indent=4))
+    print(f"Status: {post_response.status_code}")
 except ValueError:
     print("Non-JSON response received.")
     print(post_response.text)
-print(f"Status: {post_response.status_code}")
