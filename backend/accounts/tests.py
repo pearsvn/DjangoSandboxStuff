47bad8ae488895bc5cce126de6db1b6299059c65
Alfie Pearson <alfie@Alfies-MacBook-Air.local>
2025-07-15 08:58:36 +0100

    custom permissions tests

--- backend/accounts/tests.py
+++ backend/accounts/tests.py
@@ -1,9 +1,14 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
 from django.urls import reverse
+from django.http import HttpRequest
 
+from rest_framework.request import Request
 from rest_framework.test import APIClient, APITestCase
 from rest_framework import status
+
+from accounts.permissions import IsSuperUserOrAuthenticatedUser
+
 
 User = get_user_model()
 
@@ -38,3 +43,71 @@ class LoginTests(APITestCase):
         }, format='json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertIn('error', response.data)
+
+class PermissionsTests(APITestCase):
+    """
+    Tests for the custom permissions class found in the permissions file
+    """
+    def setUp(self):
+        # Create a user to authenticate
+        self.user = User.objects.create_user(username="loginuser", password="pass1234")
+        self.superuser = User.objects.create_superuser(username="loginsuperuser", password="superpass")
+        self.client = APIClient()
+        self.url = reverse('login')
+    
+    def test_authenticated_user_accessing_own_object(self):
+        # Simulate user accessing their own object
+        class DummyObj:
+            owner = self.user
+
+        permission = IsSuperUserOrAuthenticatedUser()
+
+        request = Request(HttpRequest())
+        request.user = self.user
+
+        self.client.force_authenticate(user=self.user)
+        self.assertTrue(permission.has_object_permission(
+            request=request, view=None, obj=DummyObj()))
+
+    def test_authenticated_user_accessing_other_object(self):
+        other_user = User.objects.create_user(username="otheruser", password="pass1234")
+
+        class DummyObj:
+            owner = other_user
+
+        permission = IsSuperUserOrAuthenticatedUser()
+
+        request = Request(HttpRequest())
+        request.user = self.user
+
+        self.assertFalse(permission.has_object_permission(
+            request=request, view=None, obj=DummyObj()))
+
+    def test_superuser_accessing_non_superuser_object(self):
+        self.client.force_authenticate(user=self.superuser)
+
+        class DummyObj:
+            owner = self.user
+
+        permission = IsSuperUserOrAuthenticatedUser()
+
+        request = Request(HttpRequest())
+        request.user = self.superuser
+
+        self.assertTrue(permission.has_object_permission(
+            request=request, view=None, obj=DummyObj()))
+
+    def test_superuser_accessing_superuser_object(self):
+        self.superuser2 = User.objects.create_superuser(username="adminuser2", password="adminpass2")
+        self.client.force_authenticate(user=self.superuser)
+
+        class DummyObj:
+            owner = self.superuser2
+
+        permission = IsSuperUserOrAuthenticatedUser()
+
+        request = Request(HttpRequest())
+        request.user = self.superuser
+
+        self.assertFalse(permission.has_object_permission(
+            request=request, view=None, obj=DummyObj()))
